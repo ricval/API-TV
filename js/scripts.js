6df9d6f6d1fcf3d5437e0d970692e9533e53d029
Lucía Aranda <84819291+aranda90@users.noreply.github.com>
2021-08-19 16:37:06 -0500

    Plantilla para mostrar listas de acuerdo - en proceso

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,38 @@
-/* boostrap 5 */
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+// Poner pantalla en fullscreen
+document.getElementById("elem").requestFullscreen();
+
+function SetFullScreen(elto) {
+    
+    // Si no se soporta la API, ya ni lo intentamos
+    if( !FullScreenSupportEnable() ) return;
+
+    // Se prueba la variante aporopiada según el navegador
+
+    try{
+        if(elto.requestFullscreen) { // Empezando por la estándar
+            
+            elto.requestFullscreen();
+
+        }else if(elto.webkitRequestFullScreeen){ // Webkit (Safari, Chrome  y Opera 15+)
+
+            elto.webkitRequestFullScreeen();
+        }else if(elto.mozRequestFullScreen){ // Firefox
+
+            elto.mozRequestFullScreen();
+        }else if(elto.msRequestFullScreen){ // Internet Explorer 11+
+
+            elto.msRequestFullScreen();
+
+        }
+    }
+    catch(ex){
+
+        return false;
+    }
+    return true;
+
+    
+}
+
+SetFullScreen(document.getElementById("elem"));
