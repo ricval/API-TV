0f3c8f5f9a3b25fa192689cae4cb8ab9da636831
aranda90 <lucyarandal@gmail.com>
2021-08-25 22:30:13 -0500

    Carga de datos mediante json

--- js/listas-de-acuerdos.js
+++ js/listas-de-acuerdos.js
@@ -7,47 +7,56 @@ $(document).ready(function() {
 });
 
 
-function consulta(id = 0, anio = 0) {
-    switch ("localhost"/*location.hostname*/) {
-        case "localhost":
-            // Para desarrollo
-            listas_plataforma_web_api_url = "http://justicia:8001/listas_de_acuerdos_acuerdos?lista_de_acuerdo_id=" + id;
-            break;
-        case "127.0.0.1":
-            // Para desarrollo
-            listas_plataforma_web_api_url = "http://172.30.37.233:8001/listas_de_acuerdos?autoridad_id=" + id + '&ano=' + anio;
-            break;
-        case "172.30.37.233":
-            // Para desarrollo
-            listas_plataforma_web_api_url = "http://172.30.37.233:8001/listas_de_acuerdos?autoridad_id=" + id + '&ano=' + anio;
-            break;
-        default:
-            // Para producción
-            listas_plataforma_web_api_url = "https://plataforma-web-api-dot-pjecz-268521.uc.r.appspot.com/listas_de_acuerdos?autoridad_id=" + id + '&ano=' + anio;
-    }
-}
 
 function resultadoConsulta(autoridad, anio) {
-    consulta(autoridad, anio);
+    //consulta(autoridad, anio);
     var nombreDistrito = "";
     var nombreAutoridad = "";
     
-    $.ajax({
-        'url': listas_plataforma_web_api_url,
-        'type': "GET",
-        'dataType': "json",
-        'success': function(result) {
-            $.each(result, function(i, lista) {
-                if (i < 3) {
-                    nombreAutoridad = lista.autoridad;
-                    nombreDistrito = lista.distrito;
-                    $('#fecha').text(lista.fecha);
-                    $('#folio').text(lista.folio);
-                    $('.registro').clone().appendTo( $(".container") );
-                    return;
-                }
-            });
-            $('#divcargando').hide();
-        }
-    });
+    fetch('js/lista_acuerdo.json')
+    .then(res => res.json())
+    .then(data => {
+        $.each(data, function(index, datos){
+            $('.loop').append(` 
+                <div style="border-bottom: solid 4px #004360" class="row">
+
+                <div style="font-size:32px;" class="col">
+        
+                <div class="row text-center pdng2" style="background-color: #dbb993; color:#7f6000;">
+        
+                    <div class="col-3 col-xs-3">
+                    
+                    <div class="row">
+                        <div  class="col-6 col-xs-6">` + datos.id + `</div>
+                        <div  class="col-6 col-xs-6">` + datos.fecha + `</div>
+                    </div>
+        
+                    </div>
+        
+                    <div class="col-3 col-xs-3">` + datos.tipo_juicio + `</div>
+                    <div class="col-3 col-xs-3">` + datos.tipo_acuerdo + `</div>
+                    <div class="col-3 col-xs-3"> </div>
+        
+                </div>
+        
+                <div class="row">
+        
+                    <div class="col-3 col-xs-3 pdng2 lbl" style="background-color:#004360;">Actor</div>
+                    <div class="col-9 col-xs-9 pdng2 uppercase">` + datos.actor + `</div>
+        
+                </div>
+        
+                <div class="row ">
+        
+                    <div class="col-3 col-xs-3 pdng2 lbl" style="background-color: #003045;">Demandado</div>
+                    <div class="col-9 col-xs-9 pdng2 uppercase">` + datos.demandado + `</div>
+        
+                </div>
+        
+                </div>
+        
+            </div>  
+            `)
+        });
+    })
 }
