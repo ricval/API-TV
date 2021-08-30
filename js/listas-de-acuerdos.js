676f695bc157013eb7aec1cc18b503fff299bfdc
Lucía Aranda <84819291+aranda90@users.noreply.github.com>
2021-08-30 15:52:59 -0500

    Pruebas de scroll con JS - probando

--- js/listas-de-acuerdos.js
+++ js/listas-de-acuerdos.js
@@ -27,57 +27,65 @@ function consulta(id = 0, anio = 0) {
 }
 
 function resultadoConsulta(autoridad, anio) {
-    consulta(autoridad, anio);
-    var nombreDistrito = "";
-    var nombreAutoridad = "";
     
+    consulta(autoridad, anio);
+    pausa = 0;
+
     fetch(listas_plataforma_web_api_url)
     .then(res => res.json())
     .then(data => {
-        $.each(data, function(index, datos){
+        $.each(data, function(index, datos){  
+            if(pausa < 5){
             $('.loop').append(` 
-            <div id="go" class="scroll-row">
-                <div style="border-bottom: solid 4px #004360" class="row">
-
-                <div style="font-size:32px;" class="col">
-        
-                <div class="row text-center pdng2" style="background-color: #dbb993; color:#7f6000;">
-        
-                    <div class="col-3 col-xs-3">
-                    
+                <div id="go" class="scroll-row">
+                    <div style="border-bottom: solid 4px #004360" class="row">
+                    <div style="font-size:32px;" class="col">
+            
+                    <div class="row text-center pdng2" style="background-color: #dbb993; color:#7f6000;">
+            
+                        <div class="col-3 col-xs-3">
+                        
+                        <div class="row">
+                            <div  class="col-6 col-xs-6">` + datos.id + `</div>
+                            <div  class="col-6 col-xs-6">` + datos.fecha + `</div>
+                        </div>
+            
+                        </div>
+            
+                        <div class="col-3 col-xs-3">` + datos.tipo_juicio + `</div>
+                        <div class="col-3 col-xs-3">` + datos.tipo_acuerdo + `</div>
+                        <div class="col-3 col-xs-3"> </div>
+            
+                    </div>
+            
                     <div class="row">
-                        <div  class="col-6 col-xs-6">` + datos.id + `</div>
-                        <div  class="col-6 col-xs-6">` + datos.fecha + `</div>
+            
+                        <div class="col-3 col-xs-3 pdng2 lbl" style="background-color:#004360;">Actor</div>
+                        <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.actor + `</div>
+            
                     </div>
-        
+            
+                    <div class="row ">
+            
+                        <div class="col-3 col-xs-3 pdng2 lbl" style="background-color: #003045;">Demandado</div>
+                        <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.demandado + `</div>
+            
                     </div>
-        
-                    <div class="col-3 col-xs-3">` + datos.tipo_juicio + `</div>
-                    <div class="col-3 col-xs-3">` + datos.tipo_acuerdo + `</div>
-                    <div class="col-3 col-xs-3"> </div>
-        
-                </div>
-        
-                <div class="row">
-        
-                    <div class="col-3 col-xs-3 pdng2 lbl" style="background-color:#004360;">Actor</div>
-                    <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.actor + `</div>
-        
-                </div>
-        
-                <div class="row ">
-        
-                    <div class="col-3 col-xs-3 pdng2 lbl" style="background-color: #003045;">Demandado</div>
-                    <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.demandado + `</div>
-        
-                </div>
-        
-                </div>
-        
-            </div> 
-            </div> 
-            `);
+            
+                    </div>
+            
+                </div> 
+                </div> 
+                `);  
+            }         
         });
-    })
+        
+        window.scroll(0,document.body.scrollHeight);
+        
+    
+    });
 }
+
+
+
 
