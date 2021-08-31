c2c7b707a09ac61753bf1a57072b04e39d5bf76c
Lucía Aranda <84819291+aranda90@users.noreply.github.com>
2021-08-31 08:42:29 -0500

    Actualización de loop listado de acuerdos

--- js/listas-de-acuerdos.js
+++ js/listas-de-acuerdos.js
@@ -26,66 +26,91 @@ function consulta(id = 0, anio = 0) {
     }
 }
 
-function resultadoConsulta(autoridad, anio) {
-    
+function resultadoConsulta(autoridad, anio){
     consulta(autoridad, anio);
-    pausa = 0;
+
+    $(".loop").html('<div class="row"><h1 class="text-center">cargando...</h1></div>');
+
+    var cantidad = 3
+    var inicio = 0;
+    var final = cantidad;
+    var interv = 8000;
 
     fetch(listas_plataforma_web_api_url)
-    .then(res => res.json())
-    .then(data => {
-        $.each(data, function(index, datos){  
-            if(pausa < 5){
-            $('.loop').append(` 
-                <div id="go" class="scroll-row">
-                    <div style="border-bottom: solid 4px #004360" class="row">
-                    <div style="font-size:32px;" class="col">
-            
-                    <div class="row text-center pdng2" style="background-color: #dbb993; color:#7f6000;">
-            
-                        <div class="col-3 col-xs-3">
-                        
-                        <div class="row">
-                            <div  class="col-6 col-xs-6">` + datos.id + `</div>
-                            <div  class="col-6 col-xs-6">` + datos.fecha + `</div>
-                        </div>
-            
-                        </div>
-            
-                        <div class="col-3 col-xs-3">` + datos.tipo_juicio + `</div>
-                        <div class="col-3 col-xs-3">` + datos.tipo_acuerdo + `</div>
-                        <div class="col-3 col-xs-3"> </div>
-            
-                    </div>
-            
-                    <div class="row">
-            
-                        <div class="col-3 col-xs-3 pdng2 lbl" style="background-color:#004360;">Actor</div>
-                        <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.actor + `</div>
-            
-                    </div>
-            
-                    <div class="row ">
-            
-                        <div class="col-3 col-xs-3 pdng2 lbl" style="background-color: #003045;">Demandado</div>
-                        <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.demandado + `</div>
-            
-                    </div>
-            
-                    </div>
-            
-                </div> 
-                </div> 
-                `);  
-            }         
-        });
-        
-        window.scroll(0,document.body.scrollHeight);
-        
-    
-    });
+        .then(res => res.json())
+        .then(data => {
+
+                setInterval(function(){ 
+
+                    ciclo(inicio,final,data);
+                    inicio = final;
+                    final = final + cantidad;
+
+                    if(final >= Object.keys(data).length){
+                        inicio = 0;
+                        final = cantidad;
+                    }
+
+                    
+
+                },interv);
+                    
+                
+
+            }
+        );
 }
 
+function ciclo(inicio, final, datos){
+    $('.loop').html('');
+    for(i = inicio; i < final; i++){
+        print_res(datos[i]);
+    }
+}
+
+function print_res(datos){
+    $('.loop').append(`
+        <div id="go" class="scroll-row">
+            <div style="border-bottom: solid 4px #004360" class="row">
+            <div style="font-size:32px;" class="col">
+
+            <div class="row text-center pdng2" style="background-color: #dbb993; color:#7f6000;">
+
+                <div class="col-3 col-xs-3">
+
+                <div class="row">
+                    <div  class="col-6 col-xs-6">` + datos.id + `</div>
+                    <div  class="col-6 col-xs-6">` + datos.fecha + `</div>
+                </div>
+
+                </div>
+
+                <div class="col-3 col-xs-3">` + datos.tipo_juicio + `</div>
+                <div class="col-3 col-xs-3">` + datos.tipo_acuerdo + `</div>
+                <div class="col-3 col-xs-3"> </div>
+
+            </div>
+
+            <div class="row">
+
+                <div class="col-3 col-xs-3 pdng2 lbl" style="background-color:#004360;">Actor</div>
+                <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.actor + `</div>
+
+            </div>
+
+            <div class="row ">
+
+                <div class="col-3 col-xs-3 pdng2 lbl" style="background-color: #003045;">Demandado</div>
+                <div class="col-9 col-xs-9 pdng2 uppercase col-bk">` + datos.demandado + `</div>
+
+            </div>
+
+            </div>
+
+        </div>
+        </div>
+    `);
+}
 
 
 
