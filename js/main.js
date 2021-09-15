46a686c4237156778e86bf81a0225ce8301b378c
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2021-09-15 13:53:04 -0500

    Cambios en el main

--- js/main.js
+++ js/main.js
@@ -1,45 +1,45 @@
 $(document).ready(function(){
-    
+
 
     $(".loop").html('<div class="row"><h1 class="text-center">cargando...</h1></div>');
 
     //obtener el ID en el parámetro
     var id_autoridad = get_id_aut();
-    
+
     //Agregar el nombre corto de la autoridad en el encabezado
     setname(id_autoridad);
-    
+
     //Consultar las listas de acuerdos relacionadas con la autoridad
     var id_list_aut;
-     
-    fetch(get_api_url_list_de_acu_aut(id_autoridad))
+
+    fetch(get_api_url_list_de_acu_aut(id_autoridad), {mode:'no-cors'})
     .then(res => res.json())
-    .then(data => { 
-        
+    .then(data => {
+
         // imprimir listas_de_acuerdos_acuerdos
-    
+
         var cantidad = 5; // cantidad de registros en la lista
         var inicio = 0;
         var final = cantidad;
         var interv = 8000;
 
-        fetch(get_api_url_list_acu(data[0].id))
+        fetch(get_api_url_list_acu(data[0].id), {mode:'no-cors'})
         .then(res => res.json())
         .then(result => {
-            
-                setInterval(function(){ 
-                    
+
+                setInterval(function(){
+
                     ciclo(inicio,final,result);
                     inicio = final;
                     final = final + cantidad;
-                    
+
                     if(final > Object.keys(result).length){
                         inicio = 0;
                         final = cantidad;
                     }
-                    
+
                 },interv);
-           
+
         });
     });
 });
@@ -60,13 +60,13 @@ function get_id_aut(){
 ** Establece el nombre de la autoridad de acuerdo al ID recibido en la URL
 */
 function setname(id){
-    
+
     var api_url = get_api_url_autoridades(id);
-    
+
     fetch(api_url)
     .then(res => res.json())
     .then(data => {
-        
+
         $("#myTxt").html(data.autoridad_corta + "  -  "+ data.autoridad);
     });
 }
@@ -108,14 +108,14 @@ function get_api_url_list_acu(id){
     return lista_acuerdo_acuerdo;
 }
 
-       
+
 /*
 ** Función para intercalar los colores de las filas a imprimir
 */
 function ciclo(inicio, final, datos){
     var c = 0;
     $('.loop').html('');
-    for(i = inicio; i < final; i++){   
+    for(i = inicio; i < final; i++){
         if(c % 2 == 0){
             bk_color = "#F0EFEF";
             color = "#0D0D0D";
@@ -138,7 +138,7 @@ function print_res(datos, color){
     //console.log(year)
     var n = i + 1;
     $('.loop').append(`
-        <div style="border-bottom: solid 4px #6f6s6f" class="row"> 
+        <div style="border-bottom: solid 4px #6f6s6f" class="row">
             <div style="font-size:32px;" class="col">
                 <div class="row text-center pdng2" style="background-color: ` + bk_color + `; color:` + color + `;">
                     <div class="col-1 col-xs-1" style="color:#0C0C0C; font-weight:900;">`+ n +`</div>
@@ -151,7 +151,7 @@ function print_res(datos, color){
                     <div class="col-2 col-xs-3 txt-lb">` + datos.tipo_juicio + `</div>
                     <div class="col-4 col-xs-3 txt-lb">` + datos.actor  + `</div>
                     <div class="col-3 col-xs-3 txt-lb">` + datos.demandado + ` </div>
-                </div> 
+                </div>
             </div>
         </div>
     `);
